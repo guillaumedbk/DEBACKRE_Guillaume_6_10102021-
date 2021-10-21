aaac938a552b8cc397303ade2b2627d4c0934f48
Guillaume <debackre.guillaume@gmail.com>
2021-10-21 10:30:19 +0200

    Gitignore

--- Back-end/middleware/passwordValidator.js
+++ Back-end/middleware/passwordValidator.js
@@ -12,15 +12,16 @@ passwordSchema
 .has().digits(2)                                // Must have at least 2 digits
 .has().not().spaces()                           // Should not have spaces
 .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
- 
+
+/*
 // Validate against a password string
-console.log(schema.validate('validPASS123'));
+console.log(passwordSchema.validate('validPASS123'));
 // => true
-console.log(schema.validate('invalidPASS'));
+console.log(passwordSchema.validate('invalidPASS'));
 // => false
  
 // Get a full list of rules which failed
-console.log(schema.validate('joke', { list: true }));
+console.log(passwordSchema.validate('joke', { list: true }));
 // => [ 'min', 'uppercase', 'digits' ]
-
+*/
 module.exports = passwordSchema;
