39759a53542510dc35b03b0c8089f378d585e8bf
Guillaume <debackre.guillaume@gmail.com>
2021-10-21 10:17:00 +0200

    Password validator

--- Back-end/controllers/user.js
+++ Back-end/controllers/user.js
@@ -2,24 +2,30 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const CryptoJS = require('crypto-js');
+const passwordValidator = require ('../middleware/passwordValidator');
 
 require('dotenv').config()
 
 exports.signup = (req, res, next)=>{
-  var key = CryptoJS.enc.Hex.parse(process.env.Crypto_key); 
-  var iv = CryptoJS.enc.Hex.parse(process.env.Crypto_iv); 
-  let encryptedMail = CryptoJS.AES.encrypt(req.body.email, key, { iv: iv }).toString();
-
-    bcrypt.hash(req.body.password, 10)
-    .then((hash) => {
-        const user = new User({
-            email: encryptedMail,
-            password: hash
-        });
-    user.save()
-    .then(()=> res.status(201).json({ message : 'objet enregistré' }))
-    .catch(error => res.status(400).json({ error }));
-    })
+  if(passwordValidator.validate(req.body.password)){
+    var key = CryptoJS.enc.Hex.parse(process.env.Crypto_key); 
+    var iv = CryptoJS.enc.Hex.parse(process.env.Crypto_iv); 
+    let encryptedMail = CryptoJS.AES.encrypt(req.body.email, key, { iv: iv }).toString();
+  
+      bcrypt.hash(req.body.password, 10)
+      .then((hash) => {
+          const user = new User({
+              email: encryptedMail,
+              password: hash
+          });
+      user.save()
+      .then(()=> res.status(201).json({ message : 'objet enregistré' }))
+      .catch(error => res.status(400).json({ error }));
+      })
+  }else{
+    return res.status(400).json({ message: 'Le mot de passe doit contenir au minimum: deux chiffres, une minuscule, une majuscule et être composé de minimum 8 caractères !' })
+  }
+ 
   }
 
 exports.login = (req, res, next) => {
